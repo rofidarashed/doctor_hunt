890bb5542ac664f11e306c81455066d0a65760a5
rofidarashed <rofidaelmaghraby@gmail.com>
2025-06-07 15:43:13 +0300

    custom splash screen

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:doctor_hunt/my_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
---
+++ lib/my_app.dart
@@ -0,0 +1,16 @@
+import 'package:doctor_hunt/feature/view/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen()
+    );
+  }
+}
+
